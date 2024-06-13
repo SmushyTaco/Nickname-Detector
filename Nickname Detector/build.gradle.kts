b4c1a30789f6feb6bdb70ed9bbc961b71228f5b8
Nikan Radan <github.com@alias.nikanradan.com>
2024-06-13 16:47:27 -0700

    1.21

--- Nickname Detector/build.gradle.kts
+++ Nickname Detector/build.gradle.kts
@@ -28,7 +28,7 @@ tasks {
     withType<JavaExec>().configureEach { defaultCharacterEncoding = "UTF-8" }
     withType<Javadoc>().configureEach { options.encoding = "UTF-8" }
     withType<Test>().configureEach { defaultCharacterEncoding = "UTF-8" }
-    withType<KotlinCompile>().configureEach { compilerOptions { jvmTarget = JvmTarget.valueOf("JVM_$javaVersion") } }
+    withType<KotlinCompile>().configureEach { compilerOptions.jvmTarget = JvmTarget.valueOf("JVM_$javaVersion") }
     jar {
         from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } }
         archiveClassifier = "dev"
@@ -38,7 +38,7 @@ tasks {
         filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) }
     }
     java {
-        toolchain { languageVersion = JavaLanguageVersion.of(javaVersion.toString()) }
+        toolchain.languageVersion = JavaLanguageVersion.of(javaVersion.toString())
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
         withSourcesJar()
--- Nickname Detector/src/main/kotlin/com/smushytaco/nickname_detector/UsernameSuggestionProvider.kt
+++ Nickname Detector/src/main/kotlin/com/smushytaco/nickname_detector/UsernameSuggestionProvider.kt
@@ -7,7 +7,7 @@ import net.minecraft.command.CommandSource
 import java.util.concurrent.CompletableFuture
 object UsernameSuggestionProvider: SuggestionProvider<CommandSource> {
     override fun getSuggestions(context: CommandContext<CommandSource>, builder: SuggestionsBuilder): CompletableFuture<Suggestions> {
-        for (playerName in context.source.playerNames) if (playerName.contains(context.input.split(" ").last(), true)) builder.suggest(playerName)
+        for (playerName in context.source.playerNames) if (playerName.contains(context.input.split(' ').last(), true)) builder.suggest(playerName)
         return builder.buildFuture()
     }
 }
