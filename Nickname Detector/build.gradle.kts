997087fe44a92eb267e1230b722b254f330fedb8
Nikan Radan <personal@nikanradan.com>
2024-11-24 20:21:15 -0800

    Updates

--- Nickname Detector/build.gradle.kts
+++ Nickname Detector/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("fabric-loom")
     kotlin("jvm")
     kotlin("plugin.serialization")
-    id("com.github.johnrengelman.shadow")
+    id("com.gradleup.shadow")
 }
 base.archivesName = project.extra["archives_base_name"] as String
 version = project.extra["mod_version"] as String
