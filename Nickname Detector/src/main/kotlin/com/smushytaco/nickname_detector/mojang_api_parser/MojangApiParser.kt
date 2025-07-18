c45b72d79b53ee671340875d28a1b82bd574e180
Nikan Radan <github.com@alias.nikanradan.com>
2025-07-18 00:07:46 -0700

    1.21.8

--- Nickname Detector/src/main/kotlin/com/smushytaco/nickname_detector/mojang_api_parser/MojangApiParser.kt
+++ Nickname Detector/src/main/kotlin/com/smushytaco/nickname_detector/mojang_api_parser/MojangApiParser.kt
@@ -1,22 +1,13 @@
 package com.smushytaco.nickname_detector.mojang_api_parser
 import kotlinx.serialization.json.Json
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.util.*
 object MojangApiParser {
     private val client = OkHttpClient()
     private val json = Json { ignoreUnknownKeys = true }
-    private fun executeRequestForGets(request: Request): NameAndUUID? {
-        return try {
-            client.newCall(request).execute().use { response -> response.body?.string()?.let { json.decodeFromString(NameAndUUID.serializer(), it) } }
-        } catch (_: Exception) { null }
-    }
-    fun getUuid(name: String): NameAndUUID? {
-        val request = Request.Builder().url("https://api.mojang.com/users/profiles/minecraft/$name/").build()
-        return executeRequestForGets(request)
-    }
-    fun getUsername(uuid: UUID): NameAndUUID? {
-        val request = Request.Builder().url("https://sessionserver.mojang.com/session/minecraft/profile/${uuid.toString().replace("-", "")}/").build()
-        return executeRequestForGets(request)
-    }
+    private fun executeRequestForGets(request: Request) = runCatching { client.newCall(request).execute().use { response -> json.decodeFromString(NameAndUUID.serializer(), response.body.string()) } }.getOrNull()
+    fun getUuid(name: String) = executeRequestForGets(Request(url = "https://api.mojang.com/users/profiles/minecraft/$name/".toHttpUrl()))
+    fun getUsername(uuid: UUID) = executeRequestForGets(Request(url = "https://sessionserver.mojang.com/session/minecraft/profile/${uuid.toString().replace("-", "")}/".toHttpUrl()))
 }
